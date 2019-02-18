1b20afbddb0444be957155119c3856dfc5badc1a
Сидорова Екатерина Владимировна <ekaterina_sidorova@main.russianpost.ru>
2019-02-18 14:34:17 +0300

    первый файл

---
+++ src/main/java/Base.java
@@ -0,0 +1,12 @@
+public class Base
+{
+    public static void main(String[] args) {
+    System.out.println("Hello world");
+
+    int a =5;
+    int b =7;
+    System.out.println("сумма a + b = " + (a+b));
+    }
+
+
+}
